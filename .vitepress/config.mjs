7b7ae9faf4bc0a07f645fd8d0ed97e49c72c3ebe
Iosif Chatzimichail <afonic@users.noreply.github.com>
2024-09-03 11:12:20 +0300

    Add search

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -61,6 +61,10 @@ export default defineConfig({
       },
     ],
 
+    search: {
+      provider: 'local'
+    },
+
     socialLinks: [
       { icon: 'github', link: 'https://github.com/reachweb/statamic-resrv' }
     ],
