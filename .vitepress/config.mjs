2a25f5c79b3881bdf8ba150e2ee47c588fed3cbc
Iosif Chatzimichail <afonic@users.noreply.github.com>
2024-09-12 23:50:49 +0300

    Enable sitemap

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -9,6 +9,9 @@ const antlersDef = JSON.parse(readFileSync(path.resolve(__dirname, './languages/
 export default defineConfig({
   title: "Statamic Resrv",
   cleanUrls: true,
+  sitemap: {
+    hostname: 'https://resrv.eu'
+  },
   head: [
     ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' }],
     ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' }],
